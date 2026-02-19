1d1c69d934b505c38af4ad7682726b11170eb484
Nayrix <nayrix2025@gmail.com>
2026-02-19 18:50:41 +0530

    feat: complete master ERP deployment with verified logic and docs

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["d:/aWebWork/Everly-Modish/index.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
