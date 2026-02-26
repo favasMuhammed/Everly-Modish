7e74c9deaab6d8378fc90bc8ae409101f8a2aa0c
Nayrix <nayrix2025@gmail.com>
2026-02-26 06:51:21 +0530

    Fix: Added Tailwind Safelist RegEx for dynamic element rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./*.html"],
+    safelist: [
+        {
+            pattern: /(bg|text|border|hover:border)-(indigo|emerald|cyan|blue|violet|rose|orange|amber|slate)-(50|200|600)/,
+        }
+    ],
     theme: {
         extend: {},
     },
